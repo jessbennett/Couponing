354335a943c7472a405f9c775f2515270e4af97b
Jessica <jessbennett924@gmail.com>
2018-10-31 22:29:23 -0400

    push

--- models/accountInfo.js
+++ models/accountInfo.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose');
 
-// Note: Match data access patterns for best preformance
-
 const accountInfo = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     email: String,
-    buisnessName: String, //Array []
-    password: String,
+    buisnessName: String,
+    password: String, 
     phoneNumber: String,
     city: String,
     yourPick: String,
     loggedInKey: String,
-    membershipExperationDate: [Date | String], // date or string
+    membershipExperationDate: [Date | String],
     ip: String
 })
 
